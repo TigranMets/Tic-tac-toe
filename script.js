f711655d0e93096cd97de8b76111330dd4639d12
Tigran <gvideo2007@gmail.com>
2021-12-03 14:36:38 +0400

    fixing connecting with html and js

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ let clickCount = false;
 let x;
 let o;
 
-squares.forEach((square) => square.addEventListener('click', write));
+squares.forEach(square => square.addEventListener('click', write));
 
 function write() {
     if(!clickCount){
         this.innerText = 'X';
+        x = this;
         clickCount = true;
     } else {
         this.innerText = 'O';
+        o = this;
         clickCount = false; 
     }
     debugger;
