f117b47553582160e823b45c199cc8d9ca1dd7b9
Tigran <gvideo2007@gmail.com>
2021-12-04 15:58:46 +0400

    i change the text x and o with x and o images

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let squares = document.querySelectorAll('.square');
 
 let clickCount = false;
-let x;
-let o;
+let firstItem;
+let secondItem;
 
 squares.forEach(square => square.addEventListener('click', write));
 
 function write() {
-    if(!clickCount){
-        this.innerText = 'X';
-        x = this;
+    if (!clickCount) {
+        let x = document.createElement('IMG');
+        x.src = 'https://www.vippng.com/png/full/493-4930781_letter-png-white-x-letter-png.png';
+        this.appendChild(x);
+        firstItem = this;
         clickCount = true;
     } else {
-        this.innerText = 'O';
-        o = this;
-        clickCount = false; 
+        let o = document.createElement('IMG');
+        o.src = 'https://media-s3-us-east-1.ceros.com/optiv/images/2018/10/09/0677daa19a3b6628a3bda54a769f456f/optiv-o.png?imageOpt=1&fit=bounds&width=1126';
+        this.appendChild(o);
+        secondItem = this;
+        clickCount = false;
     }
     debugger;
 }
+
+debugger;
