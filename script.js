ae92553aee165f288c9a29a23b925aac436ea25f
Tigran <gvideo2007@gmail.com>
2021-12-06 13:58:40 +0400

    adding score changer function

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let board = [
 ]
 
 let player = 'X';
+let scoreX = 0;
+let scoreO = 0;
+let scoreTie = 0;
 
 let squares = document.querySelectorAll('.square');
 
@@ -44,25 +47,39 @@ function check() {
     if (board[0].every((el) => { return el === "X" }) ||
         board[1].every((el) => { return el === "X" }) ||
         board[2].every((el) => { return el === "X" }) ||
-        board[0].every((el) => { return el === "O" }) ||
-        board[1].every((el) => { return el === "O" }) ||
-        board[2].every((el) => { return el === "O" }) ||
         board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' ||
         board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' ||
         board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X' ||
-        board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
-        board[1][0] == 'O' && board[1][2] == 'O' && board[2][2] == 'O' ||
-        board[0][0] == 'O' && board[1][2] == 'O' && board[2][2] == 'O' ||
         board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X' ||
-        board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X' ||
+        board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+        scoreChanger(++scoreX, 'x');
+    } else if (board[0].every((el) => { return el === "O" }) ||
+        board[1].every((el) => { return el === "O" }) ||
+        board[2].every((el) => { return el === "O" }) ||
+        board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
+        board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O' ||
+        board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O' ||
         board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O' ||
         board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
-        console.log('you won');
+        scoreChanger(++scoreO, 'o');
     }
     else if (clickCount >= 9) {
-        console.log('tie');
+        scoreChanger(++scoreTie);
     }
     else {
         console.log('waiting');
+    }
+}
+
+function scoreChanger(score, xo) {
+    let node = document.createElement('SPAN');
+    let textnode = document.createTextNode(`${score}`);
+    node.appendChild(textnode);
+    if (xo == 'x') {
+        document.querySelector('.scoreX').appendChild(node);
+    } else if (xo == 'o') {
+        document.querySelector('.scoreO').appendChild(node);
+    } else {
+        document.querySelector('.tie').appendChild(node);
     }
 }
